2cb0247076b8dac950ce8ed14bd13c790b520d45
Stéphane G <stephane.gronowski@gmail.com>
2018-07-30 07:55:51 +0200

    Create electron app and first action to get one file and log metadata

---
+++ index.js
@@ -0,0 +1,36 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
+const {
+    app,
+    BrowserWindow,
+    ipcMain
+} = electron;
+
+
+let mainWindow;
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: {
+            backgroundThrottling: false
+        }
+    });
+
+
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+})
+
+ipcMain.on('video:added', (event, videos) => {
+    const promise = new Promise((resolve, reject) => {
+        ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
+            resolve(metadata);
+        });
+    });
+
+    promise.then((metadata) => {
+        console.log(metadata);
+       
+    });
+});
